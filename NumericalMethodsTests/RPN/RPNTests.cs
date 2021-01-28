e24dee72e22147504b0650be82a53bd213220945
Aaron <aarlin5409@hessleacademy.com>
2021-01-28 19:34:05 +0000

    added tests for shunting yard algorithm

---
+++ NumericalMethodsTests/RPN/RPNTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NumericalMethods;
+using NumericalMethods.Data;
+using NumericalMethodsTests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumericalMethods.Tests
+{
+    [TestClass]
+    public class RPNTests
+    {
+        [TestMethod, JsonDataSource]
+        public void SYATests(string expected, string input)
+        {
+            Queue<Element> calculated = RPN.SYA(input);
+
+            Assert.AreEqual(expected, RPN.ToString(calculated));
+        }
+    }
+}
