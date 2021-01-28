886b3d1ff7db4a5ba8e0f2a12e9dfbcf3dba8788
Aaron <aarlin5409@hessleacademy.com>
2021-01-28 19:19:08 +0000

    added more tests

---
+++ NumericalMethodsTests/Data/VariableTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NumericalMethods.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NumericalMethodsTests;
+
+namespace NumericalMethods.Data.Tests
+{
+    [TestClass]
+    public class VariableTests
+    {
+        [TestMethod, JsonDataSource]
+        public void GetValueTests(double expected, string symbol)
+        {
+            Variable var = new(symbol);
+
+            Assert.AreEqual(expected, var.GetValue().Value, 1e-6);
+        }
+    }
+}
