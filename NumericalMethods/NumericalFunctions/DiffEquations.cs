81e0c302bbbbd85109fec065d998d133291930d6
Aaron <aarlin5409@hessleacademy.com>
2021-01-28 22:52:55 +0000

    added first implentation of euler's method for approximating differential equations

---
+++ NumericalMethods/NumericalFunctions/DiffEquations.cs
@@ -0,0 +1,22 @@
+﻿using NumericalMethods.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NumericalMethods.NumericalFunctions
+{
+    public static class DiffEquations
+    {
+        public static double Eulers(Queue<Element> elements, double step, double x, double y, int iterations)
+        {
+            for (int i = iterations; i > 0; i--)
+            {
+                y += step * RPN.Compute(elements, x);
+                x += step;
+            }
+
+            return y;
+        }
+    }
+}
---
+++ NumericalMethodsTests/NumericalFunctions/DiffEquationsTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NumericalMethods.Data;
+using NumericalMethods.NumericalFunctions;
+using NumericalMethodsTests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumericalMethods.NumericalFunctions.Tests
+{
+    [TestClass]
+    public class DiffEquationsTests
+    {
+        [TestMethod, JsonDataSource]
+        public void EulersTests(double expected, double step, double x, double y, double iterations, string function)
+        {
+            Queue<Element> calculated = RPN.SYA(function);
+            double value = DiffEquations.Eulers(calculated, step, x, y, (int)iterations);
+
+            Assert.AreEqual(expected, value, 1e-3);
+        }
+    }
+}
