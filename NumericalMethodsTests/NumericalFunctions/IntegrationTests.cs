435dd83edf641f0aea16d1124023463163d041fa
Aaron <aarlin5409@hessleacademy.com>
2021-01-28 20:59:44 +0000

    added tests for numerical methods

---
+++ NumericalMethodsTests/NumericalFunctions/IntegrationTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NumericalMethods.Data;
+using NumericalMethods.NumericalFunctions;
+using NumericalMethodsTests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumericalMethods.NumericalFunctions.Tests
+{
+    [TestClass]
+    public class IntegrationTests
+    {
+        [TestMethod, JsonDataSource]
+        public void MidOrdinateTests(double? expected, double lowerBound, double upperBound, double numOrdinates, string function)
+        {
+            Queue<Element> calculated = RPN.SYA(function);
+            double? value = Integration.MidOrdinate(calculated, lowerBound, upperBound, numOrdinates);
+
+            if (expected.HasValue)
+                Assert.AreEqual(expected.Value, value.Value, 1e-3);
+            else
+                Assert.AreEqual(expected, value);
+        }
+
+        [TestMethod, JsonDataSource]
+        public void SimpsonsTests(double? expected, double lowerBound, double upperBound, double numOrdinates, string function)
+        {
+            Queue<Element> calculated = RPN.SYA(function);
+            double? value = Integration.Simpsons(calculated, lowerBound, upperBound, numOrdinates);
+
+            if (expected.HasValue)
+                Assert.AreEqual(expected.Value, value.Value, 1e-3);
+            else
+                Assert.AreEqual(expected, value);
+        }
+    }
+}
