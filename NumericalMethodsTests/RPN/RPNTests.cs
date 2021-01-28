251749436ea22f91f4c07bd8bc0ae9c64770b8b0
Aaron <aarlin5409@hessleacademy.com>
2021-01-28 20:20:22 +0000

    added more tests for reverse polish notation code

--- NumericalMethodsTests/RPN/RPNTests.cs
+++ NumericalMethodsTests/RPN/RPNTests.cs
@@ -20,5 +20,31 @@ namespace NumericalMethods.Tests
 
             Assert.AreEqual(expected, RPN.ToString(calculated));
         }
+
+        [TestMethod, JsonDataSource]
+        public void ComputeTests(double expected, string input, double xValue = 0)
+        {
+            Queue<Element> elements = new();
+            foreach (string token in input.Split(' '))
+            {
+                if (double.TryParse(token, out double number))
+                    elements.Enqueue(new Value(number));
+                else if (OperatorsHelper.TryParse(token, out Operators op))
+                    elements.Enqueue(new Operator(op));
+                else
+                    elements.Enqueue(new Variable(token));
+            }
+
+            double value = RPN.Compute(elements, xValue);
+            Assert.AreEqual(expected, value, 1e-6);
+        }
+
+        [TestMethod, JsonDataSource]
+        public void RPNIntegrationTests(double expected, string input, double xValue = 0)
+        {
+            Queue<Element> calculated = RPN.SYA(input);
+
+            Assert.AreEqual(expected, RPN.Compute(calculated, xValue), 1e-6);
+        }
     }
 }
